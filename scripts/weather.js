4baf6032c0dd9969b33eac3a9636c876d665aadc
kylebowser <kylebowser9@gmail.com>
2026-01-22 19:19:10 -0700

    Week 3 class work

---
+++ scripts/weather.js
@@ -0,0 +1,23 @@
+49.7706695112242, 6.641893230214196
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.77&lon=6.64&appid=e66129b6c789dd6105a6da327ca0989d';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // testing only
+            // displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
